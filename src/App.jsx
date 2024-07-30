874b29030ad6d0fc01fc8845ee3cf54d458c2f56
10uay <louay.deeb45@gmail.com>
2024-07-30 14:07:08 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from './MyComponents/Hero'
+import { Knowledage } from './MyComponents/Knowledage'
+import { Navbar } from './MyComponents/Navbar'
+import { Projects } from './MyComponents/Projects'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Knowledage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import themeSlice from "./theme/themeSlice";
+
+// Define the root reducer
+const rootReducer = combineReducers({
+  theme: themeSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export const persistor = persistStore(store);
+
+
